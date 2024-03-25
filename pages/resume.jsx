740aed832015c3e9c3f5035a11173f55755f7e5e
Javoh <117713938+mugambijavan@users.noreply.github.com>
2024-03-25 18:01:06 +0300

    resume

--- pages/resume.jsx
+++ pages/resume.jsx
@@ -38,7 +38,7 @@ const resume = () => {
         <div className='text-center py-4 text-xl font-bold uppercase tracking-wider'>
           <div className='hidden sm:block'>
             <p>
-              Software Engineer <span className='px-1'>|</span> Full-Stack Developer{' '}
+              Software Engineer{' '}
             </p>
           </div>
           
