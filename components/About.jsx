c3acdd6390ad29e175c5d8c525633abe991dc112
Javoh <117713938+mugambijavan@users.noreply.github.com>
2024-06-12 21:37:12 +0300

    Update About.jsx

--- components/About.jsx
+++ components/About.jsx
@@ -8,10 +8,18 @@ const About = () => {
     <div id='about' className='w-full md:h-screen p-2 flex items-center py-16'>
       <div className='max-w-[1240px] m-auto md:grid grid-cols-3 gap-8'>
         <div className='col-span-2'>
-          <h2 className='py-4' style={{ fontSize: '17px' }}>My objective is to create products and services that consistently satisfy customers.</h2>
-          <p className='py-2 text-gray-600'>
-          I am a Software Engineer with a passion for building elegant and user-centric experiences. I bring a diverse skillset to the table, proficient in both front-end (HTML, CSS, JavaScript frameworks like React & Next.js) and back-end development (Ruby on Rails, SQL). My experience extends to mobile app development with Flutter/Dart, ensuring I can deliver solutions across platforms. Version control with Git and a collaborative workflow using GitHub keep my projects organized and maintainable.  Beyond technical expertise, I value clear communication and exceeding client expectations through a commitment to high-quality software and continuous learning. I'm eager to contribute my skills and enthusiasm to your team, fostering growth and success.  
-          </p>
+          <div className='col-span-3 w-full h-auto shadow-xl shadow-gray-400 rounded-xl lg:p-4'>
+            <div className='p-4'>
+            <iframe
+              src='https://portfoliojavoh.vercel.app/'
+              title='Embedded Website'
+              width='100%'
+              height='100%'  // You can adjust the height as needed
+              frameBorder='0'
+              allowFullScreen
+            ></iframe>
+            </div>
+          </div>
           <Link href='/#projects'>
             <p className='py-2 text-gray-600 underline cursor-pointer'>
               Check out some of my latest projects.
