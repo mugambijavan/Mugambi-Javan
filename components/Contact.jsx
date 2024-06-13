163031033fdd0b5b15b95d3cc90489f4dbdc13fd
Javoh <117713938+mugambijavan@users.noreply.github.com>
2024-06-13 21:00:40 +0300

    Update Contact.jsx

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -12,9 +12,9 @@ const Contact = () => {
     <div id='contact' className='w-full lg:h-screen'>
       <div className='max-w-[1240px] m-auto px-2 py-16 w-full '>
         <p className='text-xl tracking-widest uppercase text-[#5651e5]'>
-          Contact
+          Hello World
         </p>
-        <h2 className='py-4'>Get In Touch</h2>
+        <h2 className='py-4'>Dream it. Chase It. Code It.</h2>
         <div className='grid lg:grid-cols-5 gap-8'>
           {/* left */}
           <div className='col-span-3 lg:col-span-2 w-full h-full shadow-xl shadow-gray-400 rounded-xl p-4'>
