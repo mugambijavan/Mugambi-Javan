0ea1a43518b00eb10604c1c88ebc9cfe65b2ca5c
Javoh <117713938+mugambijavan@users.noreply.github.com>
2024-06-13 21:08:51 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
     <div>
       <Head>
         <title>Mugambi Javan</title>
-        <meta name="description" content="I’m a front-end web developer specializing in building (and occasionally designing) exceptional digital experiences." />
+        <meta name="description" content="I’m a Software Engineer" />
         <link rel="icon" href="/fav.png" />
       </Head>
     <Main />
