1f2b68c0c1947eccfd8d08c3785d4254b445792a
Javoh <117713938+mugambijavan@users.noreply.github.com>
2024-06-20 19:29:02 +0300

    Update About.jsx

--- components/About.jsx
+++ components/About.jsx
@@ -12,7 +12,11 @@ const About = () => {
             <div className='p-4'>
             <h2 className='py-4' style={{ fontSize: '17px' }}>My objective is to create products and services that consistently satisfy customers.</h2>
           <p className='py-2 text-gray-600'>
-          I am a Software Engineer with a passion for building elegant and user-centric experiences. I bring a diverse skillset to the table, proficient in both front-end (HTML, CSS, JavaScript frameworks like React & Next.js) and back-end development (Ruby on Rails, SQL). My experience extends to mobile app development with Flutter/Dart, ensuring I can deliver solutions across platforms. Version control with Git and a collaborative workflow using GitHub keep my projects organized and maintainable.  Beyond technical expertise, I value clear communication and exceeding client expectations through a commitment to high-quality software and continuous learning. I'm eager to contribute my skills and enthusiasm to your team, fostering growth and success.  
+            Software Engineer with a passion for building elegant and user-centric experiences. I bring a diverse skillset to the table, proficient in both front-end 
+            (HTML, CSS, JavaScript frameworks like React & Next.js) and back-end development (Ruby on Rails, SQL). Additionally, I possess strong project management skills,
+            adept at planning, organizing, and leading software development projects to successful completion. Version control with Git and a collaborative workflow using
+            GitHub keep my projects organized and maintainable. Beyond technical expertise, I value clear communication and exceeding client expectations through a commitment 
+            to high-quality software and continuous learning. I'm eager to contribute my skills and enthusiasm to your team, fostering growth and success.
           </p>
               <iframe
               src='https://mugambijavan.github.io/badge/'
