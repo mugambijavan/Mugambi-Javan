b5e0a8d8df8ee4148f41c2f36e2e901563d5e8a2
Javoh <117713938+mugambijavan@users.noreply.github.com>
2024-06-21 00:14:50 +0300

    Update Skills.jsx

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -13,6 +13,7 @@ import Typescript from '../public/assets/skills/typescript.png';
 import Angular from '../public/assets/skills/angular.png';
 import Flutter from '../public/assets/skills/flutter.png';
 import Dart from '../public/assets/skills/dart.png';
+import Python from '../public/assets/skills/python.png';
 
 
 const Skills = () => {
@@ -127,10 +128,10 @@ const Skills = () => {
           <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
             <div className='grid grid-cols-2 gap-4 justify-center items-center'>
               <div className='m-auto'>
-                <Image src={Angular} width='64px' height='64px' alt='/' />
+                <Image src={Python} width='64px' height='64px' alt='/' />
               </div>
               <div className='flex flex-col items-center justify-center'>
-                <h3>Angular</h3>
+                <h3>Python</h3>
               </div>
             </div>
           </div>
