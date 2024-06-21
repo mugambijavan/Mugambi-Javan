fce303e6481486d1bfe4b370754dcfdb920cd3ea
Javoh <117713938+mugambijavan@users.noreply.github.com>
2024-06-21 15:07:11 +0300

    Update Contact.jsx

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -12,15 +12,15 @@ const Contact = () => {
     <div id='contact' className='w-full lg:h-screen'>
       <div className='max-w-[1240px] m-auto px-2 py-16 w-full '>
         <p className='text-xl tracking-widest uppercase text-[#5651e5]'>
-          Hello World
+          Contact
         </p>
-        <h2 className='py-4'>Dream it. Chase It. Code It.</h2>
+        <h2 className='py-4'>Get In Touch</h2>
         <div className='grid lg:grid-cols-5 gap-8'>
           {/* left */}
           <div className='col-span-3 lg:col-span-2 w-full h-full shadow-xl shadow-gray-400 rounded-xl p-4'>
             <div className='lg:p-4 h-full '>
               <div>
-                 <iframe
+                <iframe
                   src='https://portfoliojavoh.vercel.app/'
                   title='Embedded Website'
                   width='100%'
@@ -29,7 +29,64 @@ const Contact = () => {
                   allowFullScreen
                   ></iframe>
               </div>
-             </div>
+              <div>
+                <h2 className='py-2'>Mugambi Javan Nyaga</h2>
+                <p>Software Engineer</p>
+              </div>
+              <div>
+                <p className='uppercase pt-8'>Connect With Me</p>
+                <div className='flex items-center justify-between py-4'>
+                <a
+              href='https://www.linkedin.com/in/javan-mugambi-2351a81b0/'
+              target='_blank'
+              rel='noreferrer'
+            >
+              <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                <FaLinkedinIn />
+              </div>
+            </a>
+
+            <a
+              href='https://github.com/mugambijavan'
+              target='_blank'
+              rel='noreferrer'
+            >
+              <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                <FaGithub />
+              </div>
+            </a>
+
+            <a
+              href='mailto:mugambijavan@gmail.com'
+              target='_blank'
+              rel='noreferrer'
+            >
+              <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                <AiOutlineMail />
+              </div>
+            </a>
+
+            <a
+              href='https://twitter.com/mugambi_javan'
+              target='_blank'
+              rel='noreferrer'
+            >
+              <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                <FaTwitter />
+              </div>
+            </a>
+
+            <a
+              href='https://t.me/KamiriJavan'
+              target='_blank'
+              rel='noreferrer'
+            >
+              <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                <FaTelegram />
+              </div>
+            </a>
+
+                </div>
               </div>
             </div>
           </div>
