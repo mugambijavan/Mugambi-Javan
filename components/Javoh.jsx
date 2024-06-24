57ade93231b68c27dc7da85a39eb2174bbbbb8b9
Javoh <117713938+mugambijavan@users.noreply.github.com>
2024-06-24 12:17:13 +0300

    Create Javoh.jsx

---
+++ components/Javoh.jsx
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+import React from 'react';
+
+const Javoh = () => {
+  return (
+    <div id='home' className='w-full h-screen text-center'>
+      <div className='max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center'>
+          
+      </div>
+    </div>
+  );
+};
+
+export default Javoh;
