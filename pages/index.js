59d7c4bd6a3ff1eb8e1b628895dba0cfdf35ba85
Javoh <117713938+mugambijavan@users.noreply.github.com>
2024-06-24 12:44:31 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import Head from 'next/head'
 import About from '../components/About'
-import Contact from '../components/Contact'
 import Main from '../components/Main'
 import Projects from '../components/Projects'
 import Skills from '../components/Skills'
@@ -19,7 +18,6 @@ export default function Home() {
     <About />
     <Skills />
     <Projects />
-    <Contact />
     <Javoh />
     </div>
   )
