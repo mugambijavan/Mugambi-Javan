16d42590bfc6417a4602d55ee614175617de3cc9
Javoh <117713938+mugambijavan@users.noreply.github.com>
2024-09-10 17:14:42 +0300

    Update Javoh.jsx

--- components/Javoh.jsx
+++ components/Javoh.jsx
@@ -14,17 +14,6 @@ const Javoh = () => {
         allowFullScreen
         ></iframe>
       </div>
-      
-      <div className='max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center'>
-        <iframe
-        src='https://mugambijavan.netlify.app/'
-        title='Embedded Website'
-        width='100%'
-        height='800'  // You can adjust the height as needed
-        frameBorder='0'
-        allowFullScreen
-        ></iframe>
-      </div>
       <div className='max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center'>
          <iframe
         src='https://text-reveal-lemon.vercel.app/'
