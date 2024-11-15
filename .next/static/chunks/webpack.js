2728982cc3d8a495f975defd1d0a09206060d21f
Javoh <117713938+mugambijavan@users.noreply.github.com>
2024-11-15 15:08:42 +0300

    Remove obsolete page components and update build and pages manifest files

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a03448b82111f2e1"; }
+/******/ 		__webpack_require__.h = function() { return "5ca35dab9bbcfee6"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
