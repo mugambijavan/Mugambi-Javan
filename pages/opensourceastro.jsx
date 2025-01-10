83a300743fbc0f643ffd4a53339349f0ba1b5f7f
Javoh <117713938+mugambijavan@users.noreply.github.com>
2025-01-10 11:35:31 +0300

    Add initial project configuration and create new pages for Airtime Solutions, Open Source Astro, and Open Stove

---
+++ pages/opensourceastro.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const opensourceastro = () => {
+  return (
+    <div>opensourceastro</div>
+  )
+}
+
+export default opensourceastro
---
+++ pages/openstove.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const openstove = () => {
+  return (
+    <div>openstove</div>
+  )
+}
+
+export default openstove
