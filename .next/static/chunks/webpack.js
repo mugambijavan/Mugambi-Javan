f211ceefe2917fed6e6c1296fd4eee93243613e3
Javoh <117713938+mugambijavan@users.noreply.github.com>
2025-01-16 20:26:07 +0300

    Changed appwrite logo

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e794866ecb5cf593"; }
+/******/ 		__webpack_require__.h = function() { return "cd576a80a6781f29"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.c47b849eadaa4e38.hot-update.js
+++ .next/static/webpack/webpack.c47b849eadaa4e38.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "5c2bee54db17452f"; }
+/******/ 	__webpack_require__.h = function() { return "12ee6be3f62006c3"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
