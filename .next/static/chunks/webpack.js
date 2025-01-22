eed8554ede3146646e5ce1f5b09275f05337473e
Javoh <117713938+mugambijavan@users.noreply.github.com>
2025-01-22 21:38:42 +0300

    Update webpack hot-update files and modify pages manifest

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "45c44c15ef412661"; }
+/******/ 		__webpack_require__.h = function() { return "abf71821c5e529fe"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
